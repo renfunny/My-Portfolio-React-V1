e416ced1a5a2cad05add572dc067317eac172b4c
Renato Cordova <ren99cordova@gmail.com>
2023-02-05 14:02:03 -0500

    Finished About and Resume componens and styling, finished styling for mobile, for desktop I cannot figure out how to place footer at the very bottom of the page, will ask for feedback regarding that problem

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-
+import "../../styles/About.css";
 export default function About() {
   return (
-    <main>
+    <main className="aboutme-container">
       <h1 className="title">About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      <p className="about-text">
+        I am a highly motivated and optimistic person with a passion for
+        learning and a very strong work ethic. I have always been fascinated by
+        how things around me work which led me to study biomedical sciences for
+        my undergrad. This curiosity, as well as my constant use of technology,
+        led me to teach myself the basics of HTML, CSS, and JavaScript, and
+        fueled my decision to enroll in a coding bootcamp at the University of
+        Toronto.
+      </p>
+      <p className="about-text">
+        During my time as a bootcamp student, I honed my skills and developed a
+        strong foundation in web development. I am constantly seeking new
+        opportunitie to continue to expand my knowledge and skills. Take a look
+        at my the rest of my portfolio page to see some of the projects I have
+        worked on. If you have any questions or opportunities for collaboration,
+        don't hesitate to reach out.
       </p>
     </main>
   );
--- src/Components/Pages/Resume.jsx
+++ src/Components/Pages/Resume.jsx
@@ -1,21 +1,41 @@
 import React from "react";
-
+import "../../styles/Resume.css";
 export default function Resume() {
   return (
     <main>
-      <h1>Resume</h1>
+      <h1 className="title">Resume</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+        For the full resume please{" "}
+        <a href="https://docs.google.com/document/d/1MXZQQ4hTW0YqCGA961s-MfxRSACrChb-/edit?usp=sharing&ouid=106751600692304358518&rtpof=true&sd=true">
+          Click Here!
+        </a>
       </p>
+      <p>Front-end Development:</p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Third party APIs such as jQuery and Bootstrap</li>
+      </ul>
+      <p>Back-end Development:</p>
+      <ul>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>MySQL</li>
+        <li>MongoDB</li>
+        <li>Object-Relational-Mappers such as Sequelize and Mongoose</li>
+        <li>Familiar with Model-View-Controller architecture</li>
+        <li>Use of Agile methodology</li>
+        <li>Familiar with MERN stack</li>
+      </ul>
+      <p>Development Tools:</p>
+      <ul>
+        <li>Git</li>
+        <li>Jest unit testing</li>
+        <li>Heroku</li>
+        <li>Familiar with basic computer science concepts</li>
+      </ul>
     </main>
   );
 }
